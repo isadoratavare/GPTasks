f83c62c3c161fe892af169827f320a0e3145ee41
isadoratavare <isadoratavare85@gmail.com>
2023-06-29 23:58:02 -0300

    Adding initial routes and screens

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+
+import About from "./screens/About";
+import Home from "./screens/Home";
+import Login from "./screens/Login";
+import SignUp from "./screens/SignUp";
+import CreateBoard from "./screens/CreateBoard";
+import NotFound from "./screens/NotFound";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/create-board" element={<CreateBoard />} />
+        <Route element={NotFound} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
