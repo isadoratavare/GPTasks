b61b2d3ad298587be802ec79402dc95d8b82de09
nekojess1 <al.souza.jessica@gmail.com>
2023-08-03 17:51:08 -0300

    adjust texts and position

--- src/screens/CreateBoard/index.js
+++ src/screens/CreateBoard/index.js
@@ -7,8 +7,8 @@ import { Link, useNavigate } from "react-router-dom";
 export default function CreateBoard() {
   const [projectName, setProjectName] = useState("");
   const [duration, setDuration] = useState("");
-  const [durationUnit, setDurationUnit] = useState("dias");
-  const [participants, setParticipants] = useState("");
+  const [durationUnit, setDurationUnit] = useState("default");
+  const [participants, setParticipants] = useState(1);
   const [objective, setObjective] = useState("");
 
   const handleCreateProject = () => {
@@ -26,48 +26,57 @@ export default function CreateBoard() {
       <div className="pb-5">
         <h3 className={H3Class}>Generate a new project</h3>
 
-        <form className="space-y-4 md:space-y-6">
+        <form className="space-y-4 md:space-y-8">
           <div>
-            <label htmlFor="projectName">Project Name:</label>
+            <label htmlFor="projectName">Project Name *</label>
             <Input
               type="text"
               id="projectName"
               value={projectName}
               onChange={(e) => setProjectName(e.target.value)}
-              placeholder="Enter project name"
+              placeholder="Ex: Studies"
             />
           </div>
-          <div>
-            <label htmlFor="duration">Duration:</label>
-            <Input
-              type="number"
-              id="duration"
-              value={duration}
-              onChange={(e) => setDuration(e.target.value)}
-              placeholder="Enter duration"
-            />
-            <select
-              id="durationUnit"
-              value={durationUnit}
-              onChange={(e) => setDurationUnit(e.target.value)}
-            >
-              <option value="dias">days</option>
-              <option value="semanas">weeks</option>
-              <option value="anos">years</option>
-            </select>
+          <div className="flex flex-column"> {/* Use flex-column class to stack elements vertically */}
+            <div className="input-container">
+              <label htmlFor="duration">How long will the project last? *</label>
+              <div className="flex flex-row"> {/* Use flex flex-row class to display the elements side by side */}
+                <Input
+                  type="number"
+                  id="duration"
+                  value={duration}
+                  onChange={(e) => setDuration(e.target.value)}
+                  placeholder="Enter duration"
+                  style={{ width: "134px", marginRight: "5px" }}
+                />
+                <select
+                  id="durationUnit"
+                  value={durationUnit}
+                  onChange={(e) => setDurationUnit(e.target.value)}
+                  style={{ width: "150px" }}
+                >
+                  <option value="default" disabled>Choose the period</option>
+                  <option value="dias">days</option>
+                  <option value="semanas">weeks</option>
+                  <option value="anos">years</option>
+                </select>
+              </div>
+            </div>
+
+            <div className="input-container">
+              <label htmlFor="participants">How many people will participate? *</label>
+              <Input
+                type="number"
+                id="participants"
+                value={participants}
+                onChange={(e) => setParticipants(e.target.value)}
+                style={{ width: "150px" }}
+              />
+            </div>
           </div>
+
           <div>
-            <label htmlFor="participants">Number of Participants:</label>
-            <Input
-              type="number"
-              id="participants"
-              value={participants}
-              onChange={(e) => setParticipants(e.target.value)}
-              placeholder="Enter number of participants"
-            />
-          </div>
-          <div>
-            <label htmlFor="objective">Project Objective:</label>
+            <label htmlFor="objective">What is the main objective of the project? *</label>
             <Input
               type="text"
               id="objective"
